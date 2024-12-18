077b5ecc5885d2d795a561171983c474e0172c86
kseni-sch <468150@niuitmo.ru>
2024-12-18 11:06:25 +0300

    Initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,22 @@
 import * as vscode from 'vscode';
 
-// Хранилище истории буфера обмена
 const clipboardHistory: string[] = [];
 const MAX_HISTORY = 10;
 
 export function activate(context: vscode.ExtensionContext) {
-    // Команда для добавления текущего содержимого буфера обмена в историю
     context.subscriptions.push(
         vscode.commands.registerCommand('extension.addClipboardToHistory', async () => {
             const clipboardText = await vscode.env.clipboard.readText();
             if (clipboardText && !clipboardHistory.includes(clipboardText)) {
                 clipboardHistory.unshift(clipboardText);
                 if (clipboardHistory.length > MAX_HISTORY) {
-                    clipboardHistory.pop(); // Ограничиваем размер истории
+                    clipboardHistory.pop();
                 }
                 vscode.window.showInformationMessage('Текст добавлен в историю.');
             }
         })
     );
 
-    // Команда для выбора текста из истории
     context.subscriptions.push(
         vscode.commands.registerCommand('extension.selectFromClipboardHistory', async () => {
             const selected = await vscode.window.showQuickPick(clipboardHistory, {
